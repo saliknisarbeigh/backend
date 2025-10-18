9724efb36e2e6a424e6e75b083d101893375dc89
salik nisar <beighsalik51@gmail.com>
2025-10-18 21:27:29 +0530

    done

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
 
+if (!process.env.MONGODB_URI) {
+  console.error('Error: MONGODB_URI is not defined in environment variables');
+  process.exit(1);
+}
+
 const connectDb = async () => {
-  await mongoose.connect(
-    "mongodb+srv://saliknisar:saliknisar@cluster0.5tsex9x.mongodb.net/inspiro"
-  );
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDb;
